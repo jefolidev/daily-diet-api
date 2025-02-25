92f1ccf9e0f6d39a89c1263847d5cea24bae6e10
jefolidev <jefoliveira279@gmail.com>
2025-02-25 01:18:57 -0300

    feature: adicionado validação e confirmação de dados ao formulario de nova refeição

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,7 +3,29 @@ import { Header } from "./components/header";
 import { MealsIndex } from "./components/meals";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./components/ui/dialog";
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+
+const newMealSchema = z.object({
+  mealName: z.string(),
+  mealDescription: z.string().nullable(),
+  mealDate: z.string(),
+  mealHour: z.string(),
+  isOnDiet: z.enum(["yes", "no"])
+})
+
+type NewMeal = z.infer<typeof newMealSchema>
+
 export function App() {
+  const { handleSubmit, register } = useForm<NewMeal>({
+    resolver: zodResolver(newMealSchema)
+  })
+
+
+  function handleSubmitNewMeal(data: NewMeal) {
+    console.log(data)
+  }
 
   return (
     <div className='max-w-[720px] mx-auto my-0 flex flex-col space-y-10'>
@@ -23,27 +45,27 @@ export function App() {
                 <DialogTitle>Adicionar nova refeição</DialogTitle>
                 <DialogDescription>Insira as informações e crie a nova refeição</DialogDescription>
               </DialogHeader>
-              <form className="max-w-[28.5rem] space-y-4">
+              <form className="max-w-[28.5rem] space-y-4" onSubmit={handleSubmit(handleSubmitNewMeal)}>
 
                 <div className="flex flex-col gap-1">
                   <label className="font-medium text-lg">Nome</label>
-                  <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição" />
+                  <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição" {...register("mealName")} />
                 </div>
 
                 <div className="flex flex-col gap-1">
                   <label className="font-medium text-lg">Descrição</label>
-                  <textarea className="border rounded-sm px-3 py-3" rows={4} placeholder="Descreva o que veio na sua alimentação. Ex.:Pão integral, alface, tomate..." />
+                  <textarea className="border rounded-sm px-3 py-3" rows={4} placeholder="Descreva o que veio na sua alimentação. Ex.:Pão integral, alface, tomate..."  {...register("mealDescription")} />
                 </div>
 
                 <div className="grid grid-cols-2 gap-3">
                   <div className="flex flex-col gap-1">
                     <label className="font-medium text-lg">Data</label>
-                    <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição" />
+                    <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição"  {...register("mealDate")} />
                   </div>
 
                   <div className="flex flex-col gap-1">
                     <label className="font-medium text-lg">Hora</label>
-                    <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição" />
+                    <input className="border rounded-sm py-3 px-4" placeholder="Insira o nome da refeição"  {...register("mealHour")} />
                   </div>
 
                 </div>
@@ -55,7 +77,7 @@ export function App() {
                   <div className="flex gap-4">
 
                     <div className="relative w-full flex items-center justify-center py-4">
-                      <input type="radio" className="absolute appearance-none  bg-zinc-200/80 rounded-md size-full checked:bg-green-mid border-2 border-transparent checked:border-2 checked:border-green-dark z-0" name="diet-option" value="yes" />
+                      <input type="radio" className="absolute appearance-none  bg-zinc-200/80 rounded-md size-full checked:bg-green-mid border-2 border-transparent checked:border-2 checked:border-green-dark z-0" value="yes"  {...register("isOnDiet")} />
                       <div className="flex gap-3 items-center ">
                         <div className="size-3 rounded-full relative bg-green-800" />
                         <label className="relative font-bold text-md ">Sim</label>
@@ -63,7 +85,7 @@ export function App() {
                     </div>
 
                     <div className="relative w-full flex items-center justify-center py-4">
-                      <input type="radio" className="absolute appearance-none  bg-zinc-200/80 rounded-md size-full checked:bg-red-mid border-2 border-transparent checked:border-2 checked:border-red-dark z-0" name="diet-option" value="no" />
+                      <input type="radio" className="absolute appearance-none  bg-zinc-200/80 rounded-md size-full checked:bg-red-mid border-2 border-transparent checked:border-2 checked:border-red-dark z-0" value="no"  {...register("isOnDiet")} />
                       <div className="flex gap-3 items-center ">
                         <div className="size-3 rounded-full relative bg-red-800" />
                         <label className="relative font-bold text-md ">Não</label>
