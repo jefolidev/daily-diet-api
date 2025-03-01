496b2c46ed5f8fca3bf8288373aa69c263a22613
jefolidev <jefoliveira279@gmail.com>
2025-03-01 11:01:04 -0300

    update: adicionado validação às variáveis ambientes

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config'
 import knex, { type Knex } from 'knex'
+import { env } from '../env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
-  connection: { filename: process.env.NODE_ENV },
+  connection: { filename: env.DATABASE_URL },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
