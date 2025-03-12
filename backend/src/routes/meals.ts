b480bc90db3094990de4ef89cd10cf39e7497eb4
jefolidev <jefoliveira279@gmail.com>
2025-03-12 13:44:06 -0300

    update: adicionado sistema de autenticação e permissão em todos os endpoints

--- backend/src/routes/meals.ts
+++ backend/src/routes/meals.ts
@@ -7,111 +7,148 @@ import {
   removeMealById,
   updateMealById,
 } from '../services/meal-services'
+import { MealsRules } from '../utils/rules'
 
-const { ensureAuthenticaded } = accountMiddlewares
+const { ensureAuthenticaded, checkPermissions } = accountMiddlewares
 
 /*
   TODO: ADICIONAR O LOGIN AO FRONT END E FAZER O TOKEN FICAR REGISTRADO
 */
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', { preHandler: ensureAuthenticaded }, async (req, res) => {
-    try {
-      const meals = await getAllTheMeals()
+  app.get(
+    '/',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([MealsRules.MEALS_READ_OTHERS]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const meals = await getAllTheMeals()
 
-      console.log(req.user?.id + ' ' + req.user?.role)
+        console.log(req.user?.id + ' ' + req.user?.role)
 
-      const formattedMeals = meals.map((meal) => {
-        const formattedDate = meal.date.split('T')[0]
-        // const formattedDateString = isNaN(formattedDate.getTime()) ? null : formattedDate.toISOString()
+        const formattedMeals = meals.map((meal) => {
+          const formattedDate = meal.date.split('T')[0]
+          // const formattedDateString = isNaN(formattedDate.getTime()) ? null : formattedDate.toISOString()
 
-        return {
-          ...meal,
-          is_on_diet: Boolean(meal.is_on_diet),
-          date: formattedDate,
-          // date: formattedDateString,
+          return {
+            ...meal,
+            is_on_diet: Boolean(meal.is_on_diet),
+            date: formattedDate,
+            // date: formattedDateString,
+          }
+        })
+
+        return res.status(201).send(formattedMeals)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to GET the meals. See the error: ',
+          error,
+        )
+        throw new Error('An error ocurred while trying to GET the meals.')
+      }
+    },
+  )
+
+  app.post(
+    '/',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([MealsRules.MEALS_CREATE]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const { body } = req
+
+        const accountId = req.user?.id
+
+        console.log('account id da rota post de comidas ' + accountId)
+
+        if (!accountId) {
+          return res.code(404).send('User not found')
         }
-      })
 
-      return res.status(201).send(formattedMeals)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to GET the meals. See the error: ',
-        error,
-      )
-      throw new Error('An error ocurred while trying to GET the meals.')
-    }
-  })
+        const mealData = mealsSchema.omit({ user_id: true }).parse(body)
+        const meal = await createNewMeal(mealData, accountId)
+        const newMeal = meal.map((mealItem) => {
+          return { ...mealItem, is_on_diet: mealData.is_on_diet }
+        })
 
-  app.post('/', { preHandler: ensureAuthenticaded }, async (req, res) => {
-    try {
-      const { body } = req
-
-      const accountId = req.user?.id
-
-      console.log('account id da rota post de comidas ' + accountId)
-
-      if (!accountId) {
-        return res.code(404).send('User not found')
+        return res.status(201).send({ newMeal })
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to POST a new meal. See the error: ',
+          error,
+        )
+        throw new Error('An error ocurred while trying to POST a new meal.')
       }
+    },
+  )
 
-      const mealData = mealsSchema.omit({ user_id: true }).parse(body)
-      const meal = await createNewMeal(mealData, accountId)
-      const newMeal = meal.map((mealItem) => {
-        return { ...mealItem, is_on_diet: mealData.is_on_diet }
-      })
+  app.put(
+    '/:id',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([MealsRules.MEALS_UPDATE]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const { body } = req
+        const { id } = req.params as {
+          id: string
+        }
 
-      return res.status(201).send({ newMeal })
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to POST a new meal. See the error: ',
-        error,
-      )
-      throw new Error('An error ocurred while trying to POST a new meal.')
-    }
-  })
+        const mealsSchemaWithouUserId = mealsSchema.omit({ user_id: true })
+        const parsedMeal = mealsSchemaWithouUserId.parse(body)
 
-  app.put('/:id', async (req, res) => {
-    try {
-      const { body } = req
-      const { id } = req.params as {
-        id: string
+        const updatedMeal = await updateMealById(parsedMeal, id)
+
+        return res.status(201).send(updatedMeal)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to PUT a meal. See the error: ',
+          error,
+        )
+        throw new Error('An error ocurred while trying to PUT a meal.')
       }
+    },
+  )
 
-      const mealsSchemaWithouUserId = mealsSchema.omit({ user_id: true })
-      const parsedMeal = mealsSchemaWithouUserId.parse(body)
+  app.delete(
+    '/:id',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([MealsRules.MEALS_DELETE]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const { id } = req.params as { id: string }
+        // const { id } = params as { id: string }
 
-      const updatedMeal = await updateMealById(parsedMeal, id)
+        if (!id) {
+          return res.status(400).send('Meal ID is required')
+        }
 
-      return res.status(201).send(updatedMeal)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to PUT a meal. See the error: ',
-        error,
-      )
-      throw new Error('An error ocurred while trying to PUT a meal.')
-    }
-  })
-
-  app.delete('/:id', async (req, res) => {
-    try {
-      const { id } = req.params as { id: string }
-      // const { id } = params as { id: string }
-
-      if (!id) {
-        return res.status(400).send('Meal ID is required')
+        await removeMealById(id)
+        return res.status(200).send(`Meal with id ${id} deleted successfully `)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to DELETE the current meal. See the error: ',
+          error,
+        )
+        throw new Error(
+          'An error ocurred while trying to DELETE the current meal.',
+        )
       }
-
-      await removeMealById(id)
-      return res.status(200).send(`Meal with id ${id} deleted successfully `)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to DELETE the current meal. See the error: ',
-        error,
-      )
-      throw new Error(
-        'An error ocurred while trying to DELETE the current meal.',
-      )
-    }
-  })
+    },
+  )
 }
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -41,75 +41,102 @@ export async function usersRoutes(app: FastifyInstance) {
     },
   )
 
-  app.get('/meals', { preHandler: ensureAuthenticaded }, async (req, res) => {
-    try {
-      const accountId = req.user?.id
+  app.get(
+    '/meals',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([UserRules.USER_READ_OWNSELF]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const accountId = req.user?.id
 
-      if (!accountId) {
-        throw new Error('User not exist! Insert a valid id.')
+        if (!accountId) {
+          throw new Error('User not exist! Insert a valid id.')
+        }
+
+        const users = await getMealsFromUserById(accountId)
+
+        return res.status(200).send(users)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to GET a user. See the error: ',
+          error,
+        )
+        throw new Error('An error ocurred while trying to GET a user.')
       }
+    },
+  )
 
-      const users = await getMealsFromUserById(accountId)
+  app.post(
+    '/',
+    {
+      preHandler: [
+        checkIfEmailExists,
+        checkPermissions([UserRules.USER_CREATE]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const { body } = req
 
-      return res.status(200).send(users)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to GET a user. See the error: ',
-        error,
-      )
-      throw new Error('An error ocurred while trying to GET a user.')
-    }
-  })
+        const accountData = userAccountSchema.parse(body)
+        const userData = userSchema.parse(body)
 
-  app.post('/', { preHandler: checkIfEmailExists }, async (req, res) => {
-    try {
-      const { body } = req
+        let accountId = await findAccountIdByEmail(accountData.email)
 
-      const accountData = userAccountSchema.parse(body)
-      const userData = userSchema.parse(body)
+        if (!accountId) {
+          const newAccount = await createNewAccount(accountData)
+          accountId = newAccount.id
+        }
 
-      let accountId = await findAccountIdByEmail(accountData.email)
+        if (!accountId) {
+          throw new Error('Failed to create or retrieve account ID.')
+        }
 
-      if (!accountId) {
-        const newAccount = await createNewAccount(accountData)
-        accountId = newAccount.id
+        const newUser = await createNewUser(userData, accountId)
+
+        return res.status(201).send(newUser)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to POST a new user. See the error: ',
+          error,
+        )
+        throw new Error('An error ocurred while trying to POST a new user.')
       }
+    },
+  )
 
-      if (!accountId) {
-        throw new Error('Failed to create or retrieve account ID.')
+  app.delete(
+    '/:id',
+    {
+      preHandler: [
+        ensureAuthenticaded,
+        checkPermissions([UserRules.USER_DELETE]),
+      ],
+    },
+    async (req, res) => {
+      try {
+        const { id } = req.params as { id: string }
+        // const { id } = params as { id: string }
+
+        if (!id) {
+          return res.status(400).send('User ID is required')
+        }
+
+        await removeUserById(id)
+        return res.status(200).send(`Meal with id ${id} deleted successfully `)
+      } catch (error) {
+        console.error(
+          'An error ocurred while trying to DELETE the current user. See the error: ',
+          error,
+        )
+        throw new Error(
+          'An error ocurred while trying to DELETE the current user.',
+        )
       }
-
-      const newUser = await createNewUser(userData, accountId)
-
-      return res.status(201).send(newUser)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to POST a new user. See the error: ',
-        error,
-      )
-      throw new Error('An error ocurred while trying to POST a new user.')
-    }
-  })
-
-  app.delete('/:id', { preHandler: ensureAuthenticaded }, async (req, res) => {
-    try {
-      const { id } = req.params as { id: string }
-      // const { id } = params as { id: string }
-
-      if (!id) {
-        return res.status(400).send('User ID is required')
-      }
-
-      await removeUserById(id)
-      return res.status(200).send(`Meal with id ${id} deleted successfully `)
-    } catch (error) {
-      console.error(
-        'An error ocurred while trying to DELETE the current user. See the error: ',
-        error,
-      )
-      throw new Error(
-        'An error ocurred while trying to DELETE the current user.',
-      )
-    }
-  })
+    },
+  )
 }
